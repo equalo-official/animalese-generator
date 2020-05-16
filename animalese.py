2b9211a5c6b17a15f6ba521bd77431f94c66952a
omermikhaillk <13448403+omermikhailk@users.noreply.github.com>
2020-05-17 03:54:53 +0500

    Add whitespaces, reformat comments and use f-strings

--- animalese.py
+++ animalese.py
@@ -11,55 +11,63 @@ sounds = {}
 
 keys = [letter for letter in ascii_lowercase]
 keys.extend(['th', 'sh', ' ', '.'])
-for index,ltr in enumerate(keys):
-	num = index+1
+for index, letter in enumerate(keys):
+	num = index + 1
 	if num < 10:
-		num = '0'+str(num)
-	sounds[ltr] = './sounds/'+pitch+'/sound'+str(num)+'.wav'
+		num = f'0{num}'
+	sounds[letter] = f'./sounds/{pitch}/sound{num}.wav'
 
 if pitch == 'med':
-	rnd_factor = .35
+	rnd_factor = 0.35
 else:
-	rnd_factor = .25
+	rnd_factor = 0.25
 
 infiles = []
 
 for i, char in enumerate(stringy):
 	try:
-		if char == 's' and stringy[i+1] == 'h': #test for 'sh' sound
+		# Test for 'sh' sound
+		if char == 's' and stringy[i + 1] == 'h':
 			infiles.append(sounds['sh'])
 			continue
-		elif char == 't' and stringy[i+1] == 'h': #test for 'th' sound
+		# Test for 'th' sound
+		elif char == 't' and stringy[i + 1] == 'h':
 			infiles.append(sounds['th'])
 			continue
-		elif char == 'h' and (stringy[i-1] == 's' or stringy[i-1] == 't'): #test if previous letter was 's' or 's' and current letter is 'h'
+		# Test if previous letter was 's' or 's' and current letter is 'h'
+		elif char == 'h' and (stringy[i - 1] == 's' or stringy[i - 1] == 't'):
 			continue
 		elif char == ',' or char == '?':
 			infiles.append(sounds['.'])
 			continue
-		elif char == stringy[i-1]: #skip repeat letters
+		# Skip repeat letters
+		elif char == stringy[i - 1]:
 			continue
 	except:
 		pass
-	if not char.isalpha() and char != '.': # skip characters that are not letters or periods. 
+	# Skip characters that are not letters or periods.
+	if not char.isalpha() and char != '.':
 		continue
 	infiles.append(sounds[char])
 
 combined_sounds = None
 
 print(len(infiles))
-for index,sound in enumerate(infiles):
+for index, sound in enumerate(infiles):
 	tempsound = AudioSegment.from_wav(sound)
-	if stringy[len(stringy)-1] == '?':
-		if index >= len(infiles)*.8:
-			octaves = random.random() * rnd_factor + (index-index*.8) * .1 + 2.1 # shift the pitch up by half an octave (speed will increase proportionally)
+	if stringy[len(stringy) - 1] == '?':
+		if index >= len(infiles) * 0.8:
+			# Shift the pitch up by half an octave (speed will increase proportionally)
+			octaves = random.random() * rnd_factor + (index - index * 0.8) * 0.1 + 2.1
 		else:
 			octaves = random.random() * rnd_factor + 2.0
 	else:
-		octaves = random.random() * rnd_factor + 2.3 # shift the pitch up by half an octave (speed will increase proportionally)
+		# Shift the pitch up by half an octave (speed will increase proportionally)
+		octaves = random.random() * rnd_factor + 2.3
 	new_sample_rate = int(tempsound.frame_rate * (2.0 ** octaves))
 	new_sound = tempsound._spawn(tempsound.raw_data, overrides={'frame_rate': new_sample_rate})
-	new_sound = new_sound.set_frame_rate(44100) # set uniform sample rate
+	# Set uniform sample rate
+	new_sound = new_sound.set_frame_rate(44100)
 	combined_sounds = new_sound if combined_sounds is None else combined_sounds + new_sound
 
 
